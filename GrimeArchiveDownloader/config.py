0052f965a92fadebdc282183530c22fa1011f0f8
JakeW <jakewellsd@gmail.com>
2021-03-22 21:06:39 +0000

    Change output file names

--- GrimeArchiveDownloader/config.py
+++ GrimeArchiveDownloader/config.py
@@ -2,8 +2,8 @@ class Config():
   MAX_CRAWLER_THREADS = 15
   MAX_DOWNLOAD_THREADS = 5
   MAX_MIXES_PER_PAGE = 20
-  OUTPUT_FOLDER_NAME = 'GrimeArchiveDownloads'
-  DOWNLOADED_MIXES_FILE_NAME = 'downloadedMixes.txt'
+  OUTPUT_FOLDER_NAME = 'OUTPUT-GrimeArchiveDownloads'
+  DOWNLOADED_MIXES_FILE_NAME = 'OUTPUT-downloadedMixes.txt'
   HOME_PAGE_URL = "https://grimearchive.org/"
   MIX_TABLE_URL = "https://grimearchive.org/mixes/page/"
   MIX_DETAIL_URL = "https://grimearchive.org/mix/"
